ffa72e071af628bd35c9153b73ef00759800d124
ansuman1981 <ansuman.jaiswal@my.jcu.edu.au>
2026-03-16 19:13:54 +0800

    import kivy app starting of App size bottom botton added anchor one button added two button added

--- anchor_layout_demo.py
+++ anchor_layout_demo.py
@@ -1,2 +1,12 @@
 from kivy.app import App
 from kivy.lang import Builder
+
+class AnchorLayoutDemoApp(App):
+    """Demo of AnchorLayout"""
+    def build(self):
+        """Build the kivy app from the kv file."""
+        self.title = 'Anchor Layout Demo'
+        self.root = Builder.load_file("anchor_layout.kv")
+        return self.root
+# Create and start the app running
+AnchorLayoutDemoApp().run()
